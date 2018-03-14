14f1ab2d37954d02881c8711131a0fd720f7c2eb
queq1890 <mtmtatmat4@toki.waseda.jp>
2018-03-14 15:00:35 +0900

    finishing sample review app

--- reviewApp.py
+++ reviewApp.py
@@ -1,11 +1,9 @@
-print("レビュー数:0")
-print("[0]レビューを書く")
-print("[1]レビューを読む")
-print("[2]アプリを終了する")
-user_input = int(input())
-# インデントは2つでもいい
-# 通常は4つのインデントを使用する
+# pythonではリストオブジェクトと呼ぶ
+posts = []
+line = "\n---------------------------"
+
 def post_review():
+  # pythonでは辞書オブジェクトと呼ぶ
     post = {}
     print("ジャンルを入力してください:")
     post["genre"] = input()
@@ -13,26 +11,43 @@ def post_review():
     post["title"] = input()
     print("レビューを入力してください:")
     post["review"] = input()
-    line = "\n---------------------------"
 
     print("ジャンル : " + post["genre"] + line)
     print("タイトル : " + post["title"] + line)
     print("感想 : " + post["review"] + line)
 
+    posts.append(post)
+
 def read_review():
-    pass
+    for (num, post) in enumerate(posts):
+        print("[" + str(num) + "] :" + post["genre"] + "のレビュー")
+
+    print("見たいレビューの番号を入力してください")
+    review_input = int(input())
+    post = posts[review_input]
+
+    print("## ジャンル : " + post["genre"] + line)
+    print("## タイトル : " + post["title"] + line)
+    print("## 感想 : \n" + post["review"] + line)
 
 def end_program():
-    pass
+    exit()
 
 def exception():
     print("入力された値は無効な値です")
 
-if user_input == 0:
-    post_review()
-elif user_input == 1:
-    read_review()
-elif user_input == 2:
-    end_program()
-else:
-    exception()
+while True:
+    print("レビュー数:" + str(len(posts)))
+    print("[0]レビューを書く")
+    print("[1]レビューを読む")
+    print("[2]アプリを終了する")
+    user_input = int(input())
+
+    if user_input == 0:
+        post_review()
+    elif user_input == 1:
+        read_review()
+    elif user_input == 2:
+        end_program()
+    else:
+        exception()
--- sample.py
+++ sample.py
@@ -5,8 +5,12 @@
 #     print(sum)
 #     number += 1
 
+  # pencil_case = ['pen', 'eracer', 'compass']
+  # print(pencil_case)
+
 while True:
     user_input = input()         # ユーザーの入力
     if user_input == "exit":    # exitと入力されたら
         exit()                     # プログラムを終了
     print("ループ")
+
