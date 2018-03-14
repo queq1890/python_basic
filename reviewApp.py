75970f193eea226b29c72e26392f9b11babc4313
queq1890 <mtmtatmat4@toki.waseda.jp>
2018-03-14 14:39:57 +0900

    add while true sample

--- reviewApp.py
+++ reviewApp.py
@@ -4,23 +4,35 @@ print("[1]レビューを読む")
 print("[2]アプリを終了する")
 user_input = int(input())
 # インデントは2つでもいい
+# 通常は4つのインデントを使用する
+def post_review():
+    post = {}
+    print("ジャンルを入力してください:")
+    post["genre"] = input()
+    print("タイトルを入力してください:")
+    post["title"] = input()
+    print("レビューを入力してください:")
+    post["review"] = input()
+    line = "\n---------------------------"
+
+    print("ジャンル : " + post["genre"] + line)
+    print("タイトル : " + post["title"] + line)
+    print("感想 : " + post["review"] + line)
+
+def read_review():
+    pass
+
+def end_program():
+    pass
+
+def exception():
+    print("入力された値は無効な値です")
 
 if user_input == 0:
-  post = {}
-  print("ジャンルを入力してください:")
-  post["genre"] = input()
-  print("タイトルを入力してください:")
-  post["title"] = input()
-  print("レビューを入力してください:")
-  post["review"] = input()
-  line = "\n---------------------------"
-
-  print("ジャンル : " + post["genre"] + line)
-  print("タイトル : " + post["title"] + line)
-  print("感想 : " + post["review"] + line)
+    post_review()
 elif user_input == 1:
-  pass
+    read_review()
 elif user_input == 2:
-  pass
+    end_program()
 else:
-  print("入力された値は無効な値です")
+    exception()
---
+++ sample.py
@@ -0,0 +1,12 @@
+# sum = 0
+# number = 1
+# while number <= 10:
+#     sum += number
+#     print(sum)
+#     number += 1
+
+while True:
+    user_input = input()         # ユーザーの入力
+    if user_input == "exit":    # exitと入力されたら
+        exit()                     # プログラムを終了
+    print("ループ")
